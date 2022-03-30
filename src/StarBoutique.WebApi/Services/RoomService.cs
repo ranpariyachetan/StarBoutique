03b4742daee732dddc4646350c9f04bef19f88d1
Chetanbhai Ranpariya <chetanbhai.ranpariya@autodesk.com>
2022-03-30 22:06:49 +0800

    Added business logic in service.

--- src/StarBoutique.WebApi/Services/RoomService.cs
+++ src/StarBoutique.WebApi/Services/RoomService.cs
@@ -1,28 +1,54 @@
 using StarBoutique.WebApi.Models;
+using StarBoutique.WebApi.Repositories;
+using StarBoutique.WebApi.Exceptions;
 
 namespace StarBoutique.WebApi.Services;
 
 public interface IRoomService
 {
     IEnumerable<Room> GetAllRooms();
-    bool    UpdateRoomStatus(string? roomId, RoomStatus roomStatus);
+    void UpdateRoomStatus(string? roomId, RoomStatus roomStatus);
     Room GetRoomById(string roomId);
 }
 
 public class RoomService : IRoomService
 {
+    private IRoomRepository repository;
+    public RoomService()
+    {
+        repository = new RoomRepository();
+    }
     public IEnumerable<Room> GetAllRooms()
     {
-        return new List<Room>();
+        return repository.GetAllRooms();
     }
 
-    public bool UpdateRoomStatus(string? roomId, RoomStatus status)
+    public void UpdateRoomStatus(string? roomId, RoomStatus status)
     {
-        return true;
+        var room  = repository.GetRoomById(roomId);
+
+        if(room != null)
+        {
+            room.Status = status;
+            repository.UpdateRoom(room);
+        }
+        else
+        {
+            throw new NotFoundException("Room not found.");
+        }
     }
 
     public Room GetRoomById(string roomId)
     {
-        return new Room {Id = roomId, Status = RoomStatus.Available};
+        var room = repository.GetRoomById(roomId);
+
+        if(room != null)
+        {
+            return room;
+        }
+        else
+        {
+            throw new NotFoundException("Room not found.");
+        }
     }
 }
