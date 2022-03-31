7bcb8c8e1e0b36215c47183d559670a6fc01d34f
Chetanbhai Ranpariya <chetanbhai.ranpariya@autodesk.com>
2022-03-31 19:08:18 +0800

    Completed unit tests for RoomController.

--- src/StarBoutique.Tests/Controllers/RoomControllerTests.cs
+++ src/StarBoutique.Tests/Controllers/RoomControllerTests.cs
@@ -104,4 +104,145 @@ public class RoomControllerTests
         Assert.That(result, Is.Not.Null);
         Assert.IsInstanceOf(typeof(BadRequestObjectResult), result);
     }
+
+    [Test]
+    public void CheckoutSuccess()
+    {
+        var roomId = "Room1";
+
+        mockRoomService.Setup(service => service.UpdateRoomStatus(roomId, RoomStatus.Vacant));
+
+        var result = roomController.CheckOut(roomId);
+        Assert.That(result, Is.Not.Null);
+        Assert.IsInstanceOf(typeof(JsonResult), result);
+    }
+
+    [Test]
+    public void CheckoutFailsWhenRoomNotFound()
+    {
+        var roomId = "Room1";
+
+        mockRoomService.Setup(service => service.UpdateRoomStatus(roomId, RoomStatus.Vacant)).Throws<RoomNotFoundException>();
+
+        var result = roomController.CheckOut(roomId);
+        Assert.That(result, Is.Not.Null);
+        Assert.IsInstanceOf(typeof(BadRequestObjectResult), result);
+    }
+
+    [Test]
+    public void CheckoutFailsWhenInvalidStatusUpdate()
+    {
+        var roomId = "Room1";
+
+        mockRoomService.Setup(service => service.UpdateRoomStatus(roomId, RoomStatus.Vacant)).Throws<InvalidStatusUpdateException>();
+
+        var result = roomController.CheckOut(roomId);
+        Assert.That(result, Is.Not.Null);
+        Assert.IsInstanceOf(typeof(BadRequestObjectResult), result);
+    }
+
+    [Test]
+    public void VacantSuccess()
+    {
+        var roomId = "Room1";
+
+        mockRoomService.Setup(service => service.UpdateRoomStatus(roomId, RoomStatus.Vacant));
+
+        var result = roomController.Vacant(roomId);
+        Assert.That(result, Is.Not.Null);
+        Assert.IsInstanceOf(typeof(JsonResult), result);
+    }
+
+    public void VacantFailsWhenRoomNotFound()
+    {
+        var roomId = "Room1";
+
+        mockRoomService.Setup(service => service.UpdateRoomStatus(roomId, RoomStatus.Vacant)).Throws<RoomNotFoundException>();
+
+        var result = roomController.Vacant(roomId);
+        Assert.That(result, Is.Not.Null);
+        Assert.IsInstanceOf(typeof(BadRequestObjectResult), result);
+    }
+
+    [Test]
+    public void VacantFailsWhenInvalidStatusUpdate()
+    {
+        var roomId = "Room1";
+
+        mockRoomService.Setup(service => service.UpdateRoomStatus(roomId, RoomStatus.Vacant)).Throws<InvalidStatusUpdateException>();
+
+        var result = roomController.Vacant(roomId);
+        Assert.That(result, Is.Not.Null);
+        Assert.IsInstanceOf(typeof(BadRequestObjectResult), result);
+    }
+
+    [Test]
+    public void AvailableSuccess()
+    {
+        var roomId = "Room1";
+
+        mockRoomService.Setup(service => service.UpdateRoomStatus(roomId, RoomStatus.Available));
+
+        var result = roomController.Available(roomId);
+        Assert.That(result, Is.Not.Null);
+        Assert.IsInstanceOf(typeof(JsonResult), result);
+    }
+
+    public void AvailableFailsWhenRoomNotFound()
+    {
+        var roomId = "Room1";
+
+        mockRoomService.Setup(service => service.UpdateRoomStatus(roomId, RoomStatus.Available)).Throws<RoomNotFoundException>();
+
+        var result = roomController.Available(roomId);
+        Assert.That(result, Is.Not.Null);
+        Assert.IsInstanceOf(typeof(BadRequestObjectResult), result);
+    }
+
+    [Test]
+    public void AvailableFailsWhenInvalidStatusUpdate()
+    {
+        var roomId = "Room1";
+
+        mockRoomService.Setup(service => service.UpdateRoomStatus(roomId, RoomStatus.Available)).Throws<InvalidStatusUpdateException>();
+
+        var result = roomController.Available(roomId);
+        Assert.That(result, Is.Not.Null);
+        Assert.IsInstanceOf(typeof(BadRequestObjectResult), result);
+    }
+
+    [Test]
+    public void RepairSuccess()
+    {
+        var roomId = "Room1";
+
+        mockRoomService.Setup(service => service.UpdateRoomStatus(roomId, RoomStatus.Repair));
+
+        var result = roomController.Repair(roomId);
+        Assert.That(result, Is.Not.Null);
+        Assert.IsInstanceOf(typeof(JsonResult), result);
+    }
+
+    public void RepairFailsWhenRoomNotFound()
+    {
+        var roomId = "Room1";
+
+        mockRoomService.Setup(service => service.UpdateRoomStatus(roomId, RoomStatus.Repair)).Throws<RoomNotFoundException>();
+
+        var result = roomController.Repair(roomId);
+        Assert.That(result, Is.Not.Null);
+        Assert.IsInstanceOf(typeof(BadRequestObjectResult), result);
+    }
+
+    [Test]
+    public void RepairFailsWhenInvalidStatusUpdate()
+    {
+        var roomId = "Room1";
+
+        mockRoomService.Setup(service => service.UpdateRoomStatus(roomId, RoomStatus.Repair)).Throws<InvalidStatusUpdateException>();
+
+        var result = roomController.Repair(roomId);
+        Assert.That(result, Is.Not.Null);
+        Assert.IsInstanceOf(typeof(BadRequestObjectResult), result);
+    }
 }
